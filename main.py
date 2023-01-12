05e28feff85fde375510a71191bafd2c70ea30a2
Jonas Diete <jonas.diete@gmail.com>
2023-01-12 11:36:30 +0000

    testing api with query params

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
 
-api.add_resource(HelloWorld, "/helloworld")
+
+class HelloWorld(Resource):
+    def get(self, category):
+        return {"category": category}
+
+    def post(self):
+        return {"data": "Posted"}
+
+api.add_resource(HelloWorld, "/get-word/<string:category>")
 
 if __name__ == "__main__":
     app.run(debug=True)
