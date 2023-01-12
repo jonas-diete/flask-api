59f89c3db27f29bea66c98d49b07057c209681e6
Jonas Diete <jonas.diete@gmail.com>
2023-01-12 12:09:41 +0000

    dictionary with translations

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from random import choice
 app = Flask(__name__)
 api = Api(app)
 
-words = {"furniture": ["der Tisch", "der Stuhl", "das Regal", "das Sofa", "das Bett"], "vegetables": ["die Kartoffel", "die Paprika", "die Karotte", "die Zwiebel"]}
+words = {"furniture": {"der Tisch": "table", "der Stuhl": "chair", "das Regal": "shelves", "das Sofa": "sofa", "das Bett": "bed"}, "vegetables": {"die Kartoffel": "potato", "die Paprika": "(bell) pepper", "die Karotte": "carrot", "die Zwiebel": "onion"}}
 
 class GermanWords(Resource):
     def get(self, category):
-        return {"todays word": choice(words[category])}
+        return {"todays word": choice(list(words[category].items()))}
 
 api.add_resource(GermanWords, "/get-word/<string:category>")
 
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "get-word/furniture")
+response = requests.get(BASE + "get-word/vegetables")
 print(response.json())
