087f5d5ae307a09a78a03621520ac4962faa5f9e
Jonas Diete <jonas.diete@gmail.com>
2023-01-12 11:43:40 +0000

    get returns random word from category

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from random import choice
 
 app = Flask(__name__)
 api = Api(app)
 
-
+words = {"furniture": ["der Tisch", "der Stuhl", "das Regal", "das Sofa", "das Bett"], "vegetables": ["die Kartoffel", "die Paprika", "die Karotte", "die Zwiebel"]}
 
 class HelloWorld(Resource):
     def get(self, category):
-        return {"category": category}
-
-    def post(self):
-        return {"data": "Posted"}
+        return {"todays word": choice(words[category])}
 
 api.add_resource(HelloWorld, "/get-word/<string:category>")
 
