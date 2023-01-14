c75f27a59b649d09c8c792e32d4bc56be6b0b283
Jonas Diete <jonas.diete@gmail.com>
2023-01-14 13:05:46 +0000

    added delete request

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 from random import choice
 
 app = Flask(__name__)
 api = Api(app)
 
 word_post_args = reqparse.RequestParser()
-word_post_args.add_argument("german", type=str, help="new word in German required", required=True)
-word_post_args.add_argument("english", type=str, help="new word in English required", required=True)
+word_post_args.add_argument("german_word", type=str, help="new word in German required", required=True)
+word_post_args.add_argument("english_word", type=str, help="new word in English required", required=True)
+
+word_delete_args = reqparse.RequestParser()
+word_delete_args.add_argument("german_word", type=str, help="german word requred to delete", required=True)
 
 words = {"furniture": {"der Tisch": "table", "der Stuhl": "chair", "das Regal": "shelves", "das Sofa": "sofa", "das Bett": "bed"}, "vegetables": {"die Kartoffel": "potato", "die Paprika": "(bell) pepper", "die Karotte": "carrot", "die Zwiebel": "onion"}}
 
-def create_category_if_not_existing(category):
+def create_category_if_nonexistent(category):
     if not category in words:
         words[category] = {}
+
+def abort_if_category_doesnt_exist(category):
+    if not category in words:
+        abort(404, message="Couldn't find category...")
+
+def abort_if_word_doesnt_exist(category, german_word):
+    if not german_word in words[category]:
+        abort(404, message="Couldn't find word...")
 
 class GermanWords(Resource):
     def get(self, category): 
@@ -22,11 +33,20 @@ class GermanWords(Resource):
 
     def post(self, category): 
         args = word_post_args.parse_args()
-        create_category_if_not_existing(category)
+        create_category_if_nonexistent(category)
         # Adding word to our dictionary
-        words[category][args["german"]] = args["english"]
+        words[category][args["german_word"]] = args["english_word"]
         print(words)
         return {"added word": args}, 201
+
+    def delete(self, category):
+        args = word_delete_args.parse_args()
+        abort_if_category_doesnt_exist(category)
+        abort_if_word_doesnt_exist(category, args["german_word"])
+        del words[category][args["german_word"]]
+        print(words)
+        return "word deleted", 204
+
 
 api.add_resource(GermanWords, "/word/<string:category>")
 
--- test.py
+++ test.py
@@ -8,8 +8,12 @@ print(response.json())
 response = requests.get(BASE + "word/furniture")
 print(response.json())
 
-response = requests.post(BASE + "word/clothes", json={"german": "der Schal", "english": "scarf"})
+response = requests.post(BASE + "word/clothes", json={"german_word": "der Schal", "english_word": "scarf"})
 print(response.json())
 
-response = requests.post(BASE + "word/clothes", json={"german": "die Hose", "english": "trousers"})
+response = requests.post(BASE + "word/clothes", json={"german_word": "die Hose", "english_word": "trousers"})
 print(response.json())
+
+response = requests.delete(BASE + "word/furniture", json={"german_word": "das Bett"})
+# doesn't print correct response yet - debug
+print(response)
